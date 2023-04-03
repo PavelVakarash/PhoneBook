c06c44237003bfa141ef1bd88392e60db50075cd
Pavel Vakarash <pavelvakaras@gmail.com>
2023-04-03 10:38:23 +0200

    release2

--- src/test/java/com/ait/phonebook/HeaderTests.java
+++ src/test/java/com/ait/phonebook/HeaderTests.java
@@ -7,12 +7,23 @@ import org.testng.annotations.Test;
 public class HeaderTests extends TestBase{
 
     @Test
-    public void ElementsHeaderTest() {
-        Assert.assertTrue(isElementPresent2(By.cssSelector(".navbar-component_nav__1X_4m")));
-        Assert.assertTrue(isElementPresent(By.xpath("//h1[contains(text(),'PHONEBOOK')]")));
-        Assert.assertTrue(isElementPresent2(By.className(".active")));
-        Assert.assertTrue(isElementPresent2(By.cssSelector("[href='/about']")));
-        Assert.assertTrue(isElementPresent2(By.xpath("//a[contains(text(),'LOGIN')]")));
+    public void HeaderElementsTest() {
 
     }
+    public void HeaderElementsIsPresent() {
+        Assert.assertTrue(isElementPresent2(By.cssSelector(".navbar-component_nav__1X_4m")));
+    }
+    public void PhoneBookLogoIsPresent() {
+        Assert.assertTrue(isElementPresent(By.xpath("//h1[contains(text(),'PHONEBOOK')]")));
+    }
+    public void HomeButtonIsPresent() {
+        Assert.assertTrue(isElementPresent2(By.className(".active")));
+    }
+    public void AboutButtonIsPresent() {
+        Assert.assertTrue(isElementPresent2(By.cssSelector("[href='/about']")));
+    }
+    public void LoginButtonIsPresent() {
+        Assert.assertTrue(isElementPresent2(By.xpath("//a[contains(text(),'LOGIN')]")));
+    }
+
 }
